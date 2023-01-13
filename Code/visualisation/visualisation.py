dee6685bab028e23c578d41871a47ece68e2e6dc
CamideK55 <100591824+CamideK55@users.noreply.github.com>
2023-01-13 14:03:43 +0100

    Start visualisation

--- Code/visualisation/visualisation.py
+++ Code/visualisation/visualisation.py
@@ -7,3 +7,14 @@
 #  * This code makes visual representation of the SmartGrid model.
 #  ***************************************************************************
 
+from bokeh.plotting import figure, output_file, show
+from bokeh.palettes import magma
+import pandas as pd
+
+
+def visualize(grid):
+    graph = figure(title= "AmstelHaege")
+    data = pd.read_csv(grid)
+    color = magma(256)
+    graph.scatter(data, color=color)
+    show(graph)
